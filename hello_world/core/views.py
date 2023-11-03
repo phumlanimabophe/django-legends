cb90a3f5ab9748273799c5a237ae23e1ce6fcc9d
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-11-03 16:48:07 +0000

    m

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 def index(request):
     context = {
+<<<<<<< HEAD
         "title": "Django example",
     }
     return render(request, "index.html", context)
+=======
+        "request": request,
+        "two_plus_two":2+2
+    }
+    return render(request, "html_file2.html", context)
+>>>>>>> 4a5cfd9 (m)
